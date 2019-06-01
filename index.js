10189c5cc5d1870b7d5c89bd342024635c20d5e8
Jacob Angulo <jacobangulo@hotmail.com>
2019-06-01 04:14:39 -0700

    i

---
+++ index.js
@@ -0,0 +1,17 @@
+const server = require("express")();
+const morgan = require("morgan");
+const cors = require("cors");
+
+server.use(morgan("combined"));
+server.use(cors());
+server.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "it's working" });
+});
+
+server.listen(port, () => {
+  console.log(`running on ${port}`);
+});
